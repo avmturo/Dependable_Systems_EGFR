031abf727a27c60babd23b1ee5ec81cba1b2b797
Taylor Fowler <Taylor-Fowler@users.noreply.github.com>
2019-01-31 12:51:51 +0000

    Initial Commit

---
+++ src/DepSystems/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DepSystems.Models;
+
+namespace DepSystems.Controllers
+{
+	public class HomeController : Controller
+	{
+        [HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+        // This will be called when a HttpPost is posted with the post containing calculation details
+        [HttpPost]
+        public IActionResult Index(Calculation postedCalculation)
+        {
+            return View(postedCalculation);
+        }
+
+		public IActionResult About()
+		{
+			ViewData["Message"] = "Your application description page.";
+
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			ViewData["Message"] = "Your contact page.";
+
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ src/DepSystems/Models/Calculation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DepSystems.Models
+{
+    public class Calculation
+    {
+        public string Ethnicity { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public double CreatinLevel { get; set; }
+
+        public double Calculate()
+        {
+            //eGFR  value  in  ml/min/1.73m2 = 
+            //186  x  (Creat/  88.4)-1.154 x  (Age)-0.203 x  (0.742  if  female)  x (1.210 if blackethnicity)
+
+            //const umolToDl = need for conversion 
+
+            double eth = (Ethnicity == "1") ? 1.210 : 1.0;
+
+            double gen = (Gender == "1") ? 0.742 : 1.0;
+
+            return 186 * Math.Pow(CreatinLevel / 88.4, -1.154) * Math.Pow(Age, -0.203) * gen * eth;
+
+        }
+    }
+}
