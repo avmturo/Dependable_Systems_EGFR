6fabf66c8831e5733e2f69ce76ec729b52808c25
Taylor Fowler <Taylor-Fowler@users.noreply.github.com>
2019-03-25 12:53:31 +0000

    Small changes to the UI

--- src/DepSystems/wwwroot/js/site.js
+++ src/DepSystems/wwwroot/js/site.js
@@ -1,1 +1,15 @@
-﻿
+﻿function changeTab(viewTab, tabGroup, buttonClicked) {
+    var tabs = document.getElementsByClassName(tabGroup);
+
+    for (var i = 0; i < tabs.length; ++i) {
+        if (tabs[i].classList.contains("tab-buttons")) {
+            tabs[i].getElementsByClassName("btn-info")[0].classList.remove("btn-info");
+        }
+        else {
+            tabs[i].style.display = "none";
+        }
+    }
+
+    buttonClicked.classList.add("btn-info");
+    document.getElementById(viewTab).style.display = "block";
+}
